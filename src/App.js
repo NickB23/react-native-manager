ae7771d2df5b7ba8f1e3e606105d6d3e202d981f
Nick Bruinsma <nickbruinsma@gmail.com>
2018-02-21 21:16:04 +0100

    added navigation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import firebaseConfig from '../private/firebaseConfig';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
+
 
 class App extends Component {
   componentWillMount() {
@@ -17,7 +18,7 @@ class App extends Component {
 
     return (
       <Provider store={store}>
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
---
+++ src/Router.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Scene, Router, Actions } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Scene key="root">
+        <Scene key="login" component={LoginForm} title="Login" initial />
+        <Scene
+          onRight={() => Actions.employeeCreate()}
+          rightTitle="Add"
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees"
+          type="replace"
+        />
+        <Scene
+          key="employeeCreate"
+          component={EmployeeCreate}
+          title="Create Employee"
+        />
+      </Scene>
+    </Router>
+  );
+};
+
+export default RouterComponent;
